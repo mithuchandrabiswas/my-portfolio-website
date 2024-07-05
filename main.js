ece2631ff52b5783e7dbc393aa2ec042b33e8f27
Mithu Chandra Biswas <73749059+mithuchandrabiswas@users.noreply.github.com>
2024-07-06 01:34:13 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Typing Animation
-var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer", "Graphic Designer", "Youtuber"],
+var typed = new Typed(".typing", {
+    strings: ["", "Frontend Developer", "Web Developer", "Full Stack Developer", "MERN Stack Developer"],
     typeSpeed: 100,
     loop: true,
 })
 
 // Responsive Navbar
 const nav = document.querySelector(".nav");
-    navList = nav.querySelectorAll("li");
-    totalNavList = navList.length;
-    for(let i=0; i<totalNavList; i++){
-        const a = navList[i].querySelector("a");
-        console.log(a);
-        a.addEventListener("click", function(){
-           for(let j=0; j<totalNavList; j++){
+navList = nav.querySelectorAll("li");
+totalNavList = navList.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    console.log(a);
+    a.addEventListener("click", function () {
+        for (let j = 0; j < totalNavList; j++) {
             navList[j].querySelector("a").classList.remove("active");
-           }
-            this.classList.add("active");
-        })
-    }
+        }
+        this.classList.add("active");
+    })
+}
